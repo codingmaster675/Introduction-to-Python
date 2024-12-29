e34f03ead86212ac07839b263fdc0c04c05f2952
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-12-29 11:37:46 +0300

    Introduction tio Python

---
+++ main.py
@@ -0,0 +1,10 @@
+print("My name is Bior")
+print("I am 14 years old")
+print("I like to code")
+print(14*14)
+name="Bior"
+print(name)
+weight="50kg"
+print(weight)
+import keyword
+print(keyword.kwlist)
